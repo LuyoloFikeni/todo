4b5acce3cf6b33c6d5bfba66ee4ffc284ac832c6
Luyolo Fikeni <fikeniluyolo@gmail.com>
2024-06-01 16:45:54 +0200

    new file:   DJANGO_TODO/tasks/forms.py 	modified:   DJANGO_TODO/tasks/templates/tasks/list.html 	modified:   DJANGO_TODO/tasks/views.py

--- DJANGO_TODO/tasks/views.py
+++ DJANGO_TODO/tasks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
+
 
 def index(request):
     tasks = Task.objects.all()
-    context = {'tasks': tasks}
+    form = TaskForm()
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
